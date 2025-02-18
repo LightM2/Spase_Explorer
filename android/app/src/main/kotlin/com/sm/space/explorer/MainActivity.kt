7d3e62a49862a213afe7e8f28047cbd5b694d91b
Svitlana Mozharovska <svitlana.mozharovska@uptech.team>
2025-02-18 19:41:27 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/sm/space/explorer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sm.space.explorer.spase_explorer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:spase_explorer/app/app.dart';
+import 'package:spase_explorer/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
